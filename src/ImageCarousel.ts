b2bd9230bc87f4ba255fdc56998a13e95ec72164
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-06-10 18:42:31 +0545

    update the carousel-container width on resize

--- src/ImageCarousel.ts
+++ src/ImageCarousel.ts
@@ -3,6 +3,9 @@ export default class ImageCarousel {
   container: HTMLElement | null;
   leftBtn: HTMLButtonElement | undefined;
   rightBtn: HTMLButtonElement | undefined;
+  images: HTMLImageElement[] | undefined;
+  curIndex?: number;
+  containerWidth?: number;
   constructor(containerID: string) {
     this.containerID = containerID;
     this.container = document.getElementById(this.containerID);
@@ -11,21 +14,47 @@ export default class ImageCarousel {
       return;
     }
     this.setupContainer();
+    this.setupImages();
     this.setupButton();
+
+    window.addEventListener("resize", () => this.updateContainerWidth());
   }
 
+  updateContainerWidth(): void {
+    this.containerWidth = +getComputedStyle(this.container!).width.slice(0, -2);
+    console.log(this.containerWidth);
+  }
   setupContainer(): void {
     try {
       if (this.container != null) {
-        this.container.style.maxWidth = 400 + "px";
-        this.container.style.minHeight = 300 + "px";
-        this.container.style.border = "1px solid black";
-        this.container.style.position = "relative";
+        // this.container.style.maxWidth = 400 + "px";
+        // this.container.style.minHeight = 300 + "px";
+        // this.container.style.border = "1px solid black";
+        // this.container.style.position = "relative";
+        console.log(this.container);
       } else {
         throw new Error("container is null");
       }
     } catch (e: any) {
       console.log(e);
+    }
+  }
+
+  setupImages(): void {
+    let images: NodeListOf<HTMLImageElement> | undefined =
+      this.container?.querySelectorAll<HTMLImageElement>("img");
+    let width: number = +getComputedStyle(this.container!).width.slice(0, -2);
+    console.log(width);
+
+    if (images == undefined) {
+      alert("no images found");
+    } else {
+      this.images = Array.from(images);
+      this.curIndex = 0;
+      //   set position of each image
+      this.images.forEach((image, i) => {
+        console.log(getComputedStyle(image).width);
+      });
     }
   }
 
@@ -40,28 +69,6 @@ export default class ImageCarousel {
 
     this.leftBtn.classList.add("carousel-btn", "carousel-btn--left");
     this.rightBtn.classList.add("carousel-btn", "carousel-btn--right");
-
-    //add css properties
-    // this.leftBtn.style.position = "absolute";
-    // this.rightBtn.style.position = "absolute";
-
-    // this.leftBtn.style.top = "40%";
-    // this.rightBtn.style.top = "40%";
-
-    // this.leftBtn.style.left = "0";
-    // this.rightBtn.style.right = "0";
-
-    // this.leftBtn.style.width = "20px";
-    // this.rightBtn.style.width = "20px";
-
-    // this.leftBtn.style.height = "30px";
-    // this.rightBtn.style.height = "30px";
-
-    // this.leftBtn.style.background = "transparent";
-    // this.rightBtn.style.background = "transparent";
-
-    // this.leftBtn.style.border = "none";
-    // this.rightBtn.style.border = "none";
 
     this.leftBtn.innerHTML = `<i class="fa fa-chevron-left" aria-hidden="true"></i>`;
     this.rightBtn.innerHTML = `<i class="fa fa-chevron-right" aria-hidden="true"></i>`;
