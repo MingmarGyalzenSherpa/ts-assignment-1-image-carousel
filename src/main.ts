7d661d2571fd1b1cccb8fe77c8ef5c0d9ebfb5d7
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-06-11 09:41:57 +0545

    multiple instaance

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import ImageCarousel from "./ImageCarousel.ts";
 
-const imageCarousel1: ImageCarousel = new ImageCarousel("carousel-container1");
-// const imageCarousel2: ImageCarousel = new ImageCarousel(
-//   "carousel-container2",
-//   2000
-// );
-// const imageCarousel3: ImageCarousel = new ImageCarousel("carousel-container3");
-// const imageCarousel4: ImageCarousel = new ImageCarousel(
-//   "carousel-container4",
-//   2000
-// );
+new ImageCarousel("carousel-container1");
+new ImageCarousel("carousel-container2", 2000);
+new ImageCarousel("carousel-container3");
+new ImageCarousel("carousel-container4", 2000);
